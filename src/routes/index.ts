13ca6792ad20950507be40ae15a10237acea9bfa
requirementsoftware <requirementsoftware@gmail.com>
2024-06-06 21:36:49 +0700

    ok

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,7 +10,7 @@ routes.get(
   (request: express.Request, response: express.Response): void => {
     // This could be done by serving views ... Just quick and dirty for now :-)
     response.send(
-      '<h1>Welcome to image-processing-api</h1><p>Listening at <code><a href="/api/images">/api/images</a></code> for queries containing at least a valid filename. Optionally use both width and height to set the size...</p><p>Examples:<ul><li><a href="/api/images?filename=fjord">/api/images?filename=fjord</a></li><li><a href="/api/images?filename=fjord&width=100&height=100">/api/images?filename=fjord&width=100&height=100</a></li></ul></p>'
+      '<h1>Welcome to Image Processing API - UDACITY - CD0292</h1><p>Listening at <code><a href="/api/images">/api/images</a></code> for queries containing at least a valid filename. Optionally use both width and height to set the size...</p><p>Examples:<ul><li><a href="/api/images?filename=fjord">/api/images?filename=fjord</a></li><li><a href="/api/images?filename=fjord&width=100&height=100">/api/images?filename=fjord&width=100&height=100</a></li></ul></p>'
     );
   }
 );
